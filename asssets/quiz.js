b45c1aaf5964ff9855b8553df1496a49b092064f
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-04-22 13:13:41 -0500

    Creating a list on page with question & answers

--- asssets/quiz.js
+++ asssets/quiz.js
@@ -1,33 +1,47 @@
-/*
-WHEN the page loads, there is an introductory paragraph to the code quiz and a START button
+/* WHEN the page loads, there is an introductory paragraph to the code quiz and a START button */
 
-When a user clicks the START button than a question with four possible answers loads onto the screen
-*/
+
 var questionText = document.querySelector("#question");
-var guessOptions = document.querySelector("#guess")
-var startButton = document.querySelector("#start-button")
+var guessOptions = document.querySelector("#guess");
+var startButton = document.querySelector("#start-button");
+
+var answerList = document.createElement("ul");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+document.querySelector("#guess").appendChild(answerList);
+answerList.appendChild(li1);
+answerList.appendChild(li2);
+answerList.appendChild(li3);
+answerList.appendChild(li4);
+
 
 var questions = ["question 1?", "question 2?", "question 3?", "question 4?"];
 var answers = ["1 correct", "2 correct", "3 correct", "4 correct"];
 var nonAnswers = ["fake answer 1", "fake answer 2", "fake answer 3", "fake answer 4", "fake answer 5", "fake answer 6"];
 
+/* This function loads questions from the questions and answers array */
 function loadQuestions() {
     selectQuestion = questions[Math.floor(Math.random() * questions.length)];
     selectAnswers = answers[Math.floor(Math.random() * answers.length)];
 
     questionText.textContent = selectQuestion;
-    guessOptions.textContent = selectAnswers;
+    li1.textContent = selectAnswers;
+    li2.textContent = nonAnswers[Math.floor(Math.random() * nonAnswers.length)];
+    li3.textContent = nonAnswers[Math.floor(Math.random() * nonAnswers.length)];
+    li4.textContent = nonAnswers[Math.floor(Math.random() * nonAnswers.length)]
+
 }
 
+/* When a user clicks the START button than a question with four possible answers loads onto the screen */
 startButton.addEventListener("click", loadQuestions);
 
-/*
-Then a timer begins giving the user a certain amount of time to answer the question
+/* Then a timer begins giving the user a certain amount of time to answer the question*/
 
-If the user clicks the correct answer a new question appears on the screen, else the answer is incorrect and time is subtracted from the quiz timer
+/* If the user clicks the correct answer a new question appears on the screen, else the answer is incorrect and time is subtracted from the quiz timer */
 
-When all questions are answered OR the timer reaches 0, the game ends.
+/* When all questions are answered OR the timer reaches 0, the game ends. */
 
-When the game ends, the user can save their intitials and view their score. 
-
-*/
+/* When the game ends, the user can save their intitials and view their score. */
